67c1322ad7b7fbf31ada5aac5b98a69c1c1d3be4
Adnan Patel <adnanpatel753@gmail.com>
2026-02-16 18:53:41 +0530

    Fix docker-compose issues

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,7 +8,6 @@ variable "ami_id" {
   description = "AMI ID for EC2 instance per region"
   type        = map(string)
   default = {
-    us-east-1 = "ami-0030e4319cbf4dbf2"
-    us-east-2 = "ami-0503ed50b531cc445"
+    us-east-1 = "ami-05e9f935686cfe637"
   }
 }
